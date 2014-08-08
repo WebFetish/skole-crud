4f69efe9a79323eff599fb4b0076570e37b4155e
Crallebab <degn11@hotmail.com>
2014-08-08 18:53:14 +0200

    rettet fejl med bootstrap

--- OprerBruger.aspx.cs
+++ OprerBruger.aspx.cs
@@ -34,7 +34,22 @@ public partial class Admin_OprerBruger : System.Web.UI.Page
 
     protected void Button_Save_Click(object sender, EventArgs e)
     {
-        var salt = Guid.NewGuid().ToString();
+        var fejl = false;
+
+        if (TextBox_Name.Text == "")
+        {
+            fejl = true;
+            eName.Attributes.Add("class", "form-group has-error");
+        }
+
+        if (TextBox_Email.Text == "")
+        {
+            fejl = true;
+            eEmail.Attributes.Add("class", "form-group has-error");
+        }
+        if (!fejl)
+        {
+            var salt = Guid.NewGuid().ToString();
             var password = TextBox_Password.Text;
             UTF8Encoding encoder = new UTF8Encoding();
             SHA256Managed sha256hasher = new SHA256Managed();
@@ -43,20 +58,22 @@ public partial class Admin_OprerBruger : System.Web.UI.Page
             for (int i = 0; i < hashedDataBytes.Length; i++)
                 hash.Append(hashedDataBytes[i].ToString("X2"));
 
-        var cmd = new SqlCommand();
-        var conn = new SqlConnection(Class.ConnectionString);
-        cmd.Connection = conn;
-        cmd.CommandText = cmd.CommandText = "INSERT INTO bruger (bruger_navn, bruger_email, bruger_password, bruger_salt, fk_rolle_id, bruger_dato) VALUES (@1, @2, @3, @4, @5, @6)";
-        cmd.Parameters.AddWithValue("@1", TextBox_Name.Text);
-        cmd.Parameters.AddWithValue("@2", TextBox_Email.Text);
-        cmd.Parameters.AddWithValue("@3", hash.ToString());
-        cmd.Parameters.AddWithValue("@4", salt);
-        cmd.Parameters.AddWithValue("@5", DropDownList_Role.SelectedValue);
-        cmd.Parameters.AddWithValue("@6", DateTime.Now);
-        conn.Open();
-        cmd.ExecuteNonQuery();
-        conn.Close();
-        Session["besked"] = "Bruger oprettet.";
-        Response.Redirect("OprerBruger.aspx");
+            var cmd = new SqlCommand();
+            var conn = new SqlConnection(Class.ConnectionString);
+            cmd.Connection = conn;
+            cmd.CommandText = cmd.CommandText = "INSERT INTO bruger (bruger_navn, bruger_email, bruger_password, bruger_salt, fk_rolle_id, bruger_dato) VALUES (@1, @2, @3, @4, @5, @6)";
+            cmd.Parameters.AddWithValue("@1", TextBox_Name.Text);
+            cmd.Parameters.AddWithValue("@2", TextBox_Email.Text);
+            cmd.Parameters.AddWithValue("@3", hash.ToString());
+            cmd.Parameters.AddWithValue("@4", salt);
+            cmd.Parameters.AddWithValue("@5", DropDownList_Role.SelectedValue);
+            cmd.Parameters.AddWithValue("@6", DateTime.Now);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            Session["besked"] = "Bruger oprettet.";
+            Response.Redirect("OprerBruger.aspx");
+        }
+        
     }
 }
